f0ad7e74618172a00906faa446b24b6ff8b03f33
wonkkkn <lwg1421@naver.com>
2023-02-21 10:04:21 +0900

    도서 목록 디자인 수정

--- BookFinder/src/main/resources/static/js/pages/admin/book-list.js
+++ BookFinder/src/main/resources/static/js/pages/admin/book-list.js
@@ -1,4 +1,5 @@
-(() => {
+(function() {
+	
 	// 엘리멘트 초기화
 	const resetElem = (id) => {
 		const elem = document.getElementById(id);
@@ -9,95 +10,53 @@
 	
 	// 도서 렌더링
 	const renderBook = (book) => {
-		const card = document.createElement("div");
-		card.classList.add("card");
-		card.classList.add("flex-row");
-		card.classList.add("book-row");
 		
-		// 도서 이미지
-		const img = document.createElement("img");
-		img.classList.add("card-img-left");
-		img.classList.add("border");
-		img.classList.add("book-thumbnail");
-		if(typeof book.filename !== "undefined" && book.filename !== "" && book.filename !== null) {
-			img.setAttribute("src", "/images/uploads/" + book.filename);
-		} else {
-			img.setAttribute("src", "/images/noimg.jpg");
-		}
-		card.appendChild(img);
+				// 행
+				const row = document.createElement("tr");
+				
+				// 행의 첫번째 열-도서번호
+				const cellBookId = document.createElement("td");
+				cellBookId.textContent = book.bookId;
+				row.appendChild(cellBookId);
+				
+				// 행의 두번째 열-도서명
+				const cellBookName = document.createElement("td");
+				cellBookName.textContent = book.bookName;
+				row.appendChild(cellBookName);
+				
+				// 행의 세번째 열-저자
+				const cellAuthor = document.createElement("td");
+				cellAuthor.textContent = book.author;
+				row.appendChild(cellAuthor);
+				
+				// 행의 네번째 열-출판사
+				const cellPublisher = document.createElement("td");
+				cellPublisher.textContent = book.publisher;
+				row.appendChild(cellPublisher);
+				
+				// 행의 다섯번째 열-출판일
+				const cellPublicationDate = document.createElement("td");
+				cellPublicationDate.textContent = book.publicationDate;
+				row.appendChild(cellPublicationDate);
+				
+				// 행의 여섯번째 열-상세정보
+				const cellDetailBook = document.createElement("td");
+				//cellDetailBook.innerHTML = "<span style='color:blue'>상세정보보기</span>"
+				row.appendChild(cellDetailBook);
+				
+				const link = document.createElement("a");
+				link.innerHTML = "<td style='color:blue'>상세정보보기</td>"
+				link.classList.add("link-secondary");
+				link.classList.add("link-book-name");
+				link.setAttribute("href", "/book/" + book.bookId);
+				
+				cellDetailBook.appendChild(link);
+				
+				const target = document.getElementById("target");
+				target.appendChild(row);
+			};
 		
-		const cardBody = document.createElement("div");
-		cardBody.classList.add("card-body");
-		cardBody.classList.add("align-middle");
 		
-		// 도서명
-		const title = document.createElement("h4");
-		title.classList.add("card-title");
-		title.classList.add("book-title");
-		
-		const link = document.createElement("a");
-		link.textContent = book.bookName;
-		link.classList.add("link-secondary");
-		link.classList.add("link-book-name");
-		link.setAttribute("href", "/book/" + book.bookId);
-		
-		title.appendChild(link);
-		cardBody.appendChild(title);
-		
-		const cardText = document.createElement("div");
-		
-		// 첫번째 줄
-		const p1 = document.createElement("p");
-		p1.classList.add("mb-0");
-		
-		// 저자
-		const author = document.createElement("span");
-		author.classList.add("text-wrap");
-		author.classList.add("border-end");
-		author.classList.add("item-border-right");
-		author.textContent = "저자: " +  ( book.author !== "" && book.author !== null ? book.author : "" );  
-		p1.appendChild(author);
-		
-		// 출판사
-		const publisher = document.createElement("span");
-		publisher.classList.add("text-wrap");
-		publisher.classList.add("border-end");
-		publisher.classList.add("item-border-right");
-		publisher.textContent = "출판사: " +  ( book.publisher !== "" && book.publisher !== null ? book.publisher : "" );  
-		p1.appendChild(publisher);
-		
-		// 발행연도
-		const publicationDate = document.createElement("span");
-		publicationDate.classList.add("text-wrap");
-		publicationDate.textContent = "발행연도: " +  ( book.publicationDate !== "" && book.publicationDate !== null ? book.publicationDate.substring(0, 4) : "" );  
-		p1.appendChild(publicationDate);
-		
-		cardText.appendChild(p1);
-		
-		// 두번째 줄
-		const p2 = document.createElement("p");
-		
-		// 도서위치
-		const location = document.createElement("span");
-		location.classList.add("text-wrap");
-		location.classList.add("border-end");
-		location.classList.add("item-border-right");
-		location.innerHTML = "자료위치: " +  ( typeof book.location === 'object' && book.location.locationName !== "" && book.location.locationName !== null ? book.location.locationName : "<small class='text-muted'>(알수없음)</small>" );  
-		p2.appendChild(location);
-		
-		// 청구기호
-		const locationMemo = document.createElement("span");
-		locationMemo.classList.add("text-wrap");
-		locationMemo.innerHTML = "청구기호: " +  ( book.locationMemo !== "" && book.locationMemo !== null ? book.locationMemo : "<small class='text-muted'>(없음)</small>" );  
-		p2.appendChild(locationMemo);
-		
-		cardText.appendChild(p2);
-		
-		cardBody.appendChild(cardText);
-		card.appendChild(cardBody);
-		document.getElementById('result-area').appendChild(card);
-	};
-	
 	// 페이지네이션 렌더링
 	const renderPagination = (totalCount, keyword, condition, page) => {
 		// 페이지네이션 엘리멘트 초기화
@@ -132,6 +91,7 @@
 			document.querySelector("#pagination").appendChild(nav);
 		}
 	}
+		
 	
 	// 페이지 총 개수 표시
 	const renderTotalCount = (keyword, condition, page) => {
@@ -142,7 +102,7 @@
 		axios.get('/api/book/count', {params})
 		.then(function (response) {
 			const totalCount = response.data;
-			pageCount.textContent = "전체: " + totalCount;
+			pageCount.textContent = "전체 도서 수 : " + totalCount +"권";
 			renderPagination(totalCount, keyword, condition, page);
 		})
 		.catch(function (error) {
@@ -150,7 +110,8 @@
 			console.error(error);
 		});
 	};
-	
+		
+			
 	// 도서 검색 
 	const search = (keyword, condition, page) => {
 		
@@ -161,7 +122,7 @@
 			renderTotalCount(keyword, condition, page);
 			
 			// 검색 결과 영역 초기화
-			resetElem('result-area');
+			resetElem('target');
 			
 			// 도서 검색 결과 렌더링
 			response.data.forEach(book => renderBook(book));
@@ -172,27 +133,112 @@
 		});	
 	}
 	
-	// onload
-	document.addEventListener("DOMContentLoaded", () => {
-		// 검색 버튼 클릭시 이벤트
-		const button = document.getElementById("button-search");
-		button.addEventListener("click", () => {
+
+	// HTML 언어로 쓰여진 문자열들을 웹브라우저가 화면에 렌더링 완료했다.
+	document.addEventListener("DOMContentLoaded", function () {
+		
+		// 화면에 도서 목록 송출
+		axios
+		.get("/api/book")
+		.then(function(response) {
+			console.log("데이터 개수 : "+response.data.length+"개");
+			for(let i = 1; i<response.data.length; i++) {
+				console.log(response.data[i]);
+				const book = response.data[i];
+				// 행
+				const row = document.createElement("tr");
+				
+				// 행의 첫번째 열-도서번호
+				const cellBookId = document.createElement("td");
+				cellBookId.textContent = book.bookId;
+				row.appendChild(cellBookId);
+				
+				// 행의 두번째 열-도서명
+				const cellBookName = document.createElement("td");
+				cellBookName.textContent = book.bookName;
+				row.appendChild(cellBookName);
+				
+				// 행의 세번째 열-저자
+				const cellAuthor = document.createElement("td");
+				cellAuthor.textContent = book.author;
+				row.appendChild(cellAuthor);
+				
+				// 행의 네번째 열-출판사
+				const cellPublisher = document.createElement("td");
+				cellPublisher.textContent = book.publisher;
+				row.appendChild(cellPublisher);
+				
+				// 행의 다섯번째 열-출판일
+				const cellPublicationDate = document.createElement("td");
+				cellPublicationDate.textContent = book.publicationDate;
+				row.appendChild(cellPublicationDate);
+				
+				// 행의 여섯번째 열-상세정보
+				//const cellDetailBook = document.createElement("td");
+				//cellDetailBook.innerHTML = "<span style='color:blue'>상세정보보기</span>"
+				//row.appendChild(cellDetailBook);
+				
+				const link = document.createElement("a");
+				link.textContent = book.bookName;
+				link.classList.add("link-secondary");
+				link.classList.add("link-book-name");
+				link.setAttribute("href", "/book/" + book.bookId);
+				row.appendChild(link);
+				
+				const target = document.getElementById("target");
+				target.appendChild(row);
+			}
+			
+			// 기본적으로 페이지 뜨게끔
 			const keyword = document.getElementById("input-search-keyword").value;
 			const select = document.getElementById("select-search-condition");
 			const condition =  select.options[select.selectedIndex].value;
 			search(keyword, condition, 1);
-		});
-		
-		// 엔터키 입력시 이벤트
-		const input = document.getElementById("input-search-keyword");
-		input.addEventListener("keyup", (e) => {
-			if (e.keyCode === 13) {
-				e.preventDefault();
+			
+			// 검색 버튼 클릭시 이벤트
+			const button = document.getElementById("button-search");
+			button.addEventListener("click", () => {
+				const keyword = document.getElementById("input-search-keyword").value;
 				const select = document.getElementById("select-search-condition");
 				const condition =  select.options[select.selectedIndex].value;
-				search(input.value, condition, 1);
-			}
+				search(keyword, condition, 1);
+			});
+			
+			// 엔터키 입력시 이벤트
+			const input = document.getElementById("input-search-keyword");
+			input.addEventListener("keyup", (e) => {
+				if (e.keyCode === 13) {
+					e.preventDefault();
+					const select = document.getElementById("select-search-condition");
+					const condition =  select.options[select.selectedIndex].value;
+					search(input.value, condition, 1);
+				}
+			});
+
+		}).catch(function(error) {
 		});
-		
+
 	});
+	
+	
+	
+	
 })();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

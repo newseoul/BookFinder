766ddca95f511d1108fc5d424364d607a1129812
이미선 <2meishan@naver.com>
2023-02-22 18:07:59 +0900

    BookRestController 추가 마이페이지 도서 대출 내역 작업 완료 - 디자인 추후 작업 예정

---
+++ BookFinder/src/main/resources/static/js/pages/user/mypage.js
@@ -0,0 +1,70 @@
+(() => {
+	// 나의 도서 대출 내역
+	const bookRentalList = () => {
+		const tbody = document.querySelector("#tbody-user-rental");
+		
+		while(tbody.firstChild) {
+			tbody.removeChild(tbody.firstChild);
+		}
+		
+		const username = document.querySelector("#username").value;
+		axios.get(`/api/user/${username}/rental`)
+		.then( (response) => {
+			const books = response.data;
+			for(let i = 0; i < books.length; i++) {
+				const book = books[i];
+				
+				const tr = document.createElement("tr");
+				
+				const cellBookName = document.createElement("td");
+				cellBookName.textContent = book.bookName;
+				tr.appendChild(cellBookName);
+				
+				// 대출일시
+				const cellRentalDate = document.createElement("td");
+				cellRentalDate.textContent = book.rentalDate;
+				tr.appendChild(cellRentalDate);
+				
+				// 반납기한
+				const cellReturnDueDate = document.createElement("td");
+				cellReturnDueDate.textContent = book.returnDueDate;
+				tr.appendChild(cellReturnDueDate);
+				
+				// 반납일자
+				const cellReturnDate = document.createElement("td");
+				cellReturnDate.textContent = book.returnDate;
+				tr.appendChild(cellReturnDate);
+				
+				// 상태
+				const cellRentalStatus = document.createElement("td");
+				if(typeof book.rentalStatus === 'string') {
+					switch(book.rentalStatus) {
+						case 'on_rental':
+							cellRentalStatus.textContent = "대여중";
+							break;							
+						case 'overdue':
+							cellRentalStatus.textContent = "연체중";
+							break;
+						case 'returned':
+							cellRentalStatus.textContent = "반납완료";
+							break;
+					}
+					
+				}
+				tr.appendChild(cellRentalStatus);
+				
+				tbody.appendChild(tr);
+			}
+		})
+		.catch(error => {
+			console.error('Ajax 통신 오류');
+			console.error(error);			
+		}); // 1
+	}
+	
+	// onload
+	document.addEventListener("DOMContentLoaded", () => {
+		bookRentalList();
+	});
+	
+})();
